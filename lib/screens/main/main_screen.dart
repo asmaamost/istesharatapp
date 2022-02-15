5e05d76b975feba36942f27be186cce8fc684312
asmaamost <Asmaamostafa548@gmail.com>
2022-02-16 00:03:47 +0200

    ttt

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:istchrat/screens/main/widgets/home_header.dart';
 
 class MainScreen extends StatelessWidget {
   @override
@@ -13,7 +14,7 @@ class MainScreen extends StatelessWidget {
           height: MediaQuery.of(context).size.height,
           child: Column(
             children: [
-              homeHeader()
+              HomeHeader()
             ],
           ),
         ),
